0db2e5881d9d4ee78020f8e0707524f69a1c6fb5
carlosdfranca <carlosdudu258@gmail.com>
2020-08-17 12:25:49 -0300

    Aula 20 - 17/08
    Fiz um pouco da Aula 20 nesse dia.

---
+++ Aulas&Desafios - Python/Python/Mundo 03/Aulas/Aula20-1.py
@@ -0,0 +1,5 @@
+def título(txt):
+    print('-'*30)
+    print(txt)
+    print('-'*30)
+título('SISTEMA DE ALUNOS')
---
+++ Aulas&Desafios - Python/Python/Mundo 03/Aulas/Aula20-2.py
@@ -0,0 +1,8 @@
+def soma(a, b):
+    print(f'A = {a} e B = {b}')
+    s = a + b
+    print(f'A soma de A + B = {s}')
+ 
+soma(9, 9)
+soma(4, 5)
+soma(2, 1)
